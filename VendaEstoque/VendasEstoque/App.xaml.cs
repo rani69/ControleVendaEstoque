82eda8a99600c22d103563c252e3a28eb1d8c656
rani69 <52800504+rani69@users.noreply.github.com>
2019-10-16 15:00:19 -0300

    Create App.xaml.cs

---
+++ VendaEstoque/VendasEstoque/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms;
+
+
+namespace VendasEstoque
+{
+    public partial class Application : Xamarin.Forms.Application
+    {
+        public Application()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}

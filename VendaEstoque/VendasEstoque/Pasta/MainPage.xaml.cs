f178c19bbd628199d0b8cc216688494c55278ba0
rani69 <52800504+rani69@users.noreply.github.com>
2019-10-16 15:31:32 -0300

    Create MainPage.xaml.cs

---
+++ VendaEstoque/VendasEstoque/Pasta/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+namespace VendasEstoque
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    // [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClientes_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ConsultaClientes());
+            /* Clientes clientes = new Clientes();
+             Navigation.PushAsync(new DetalhaCliente(clientes));*/
+        }
+
+        private void BtnEstoque_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new TelaProduto());
+
+        }
+
+        private void BtnVendas_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new HistoricoVendas());
+
+        }
+
+        private void BtnSobre_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Sobre());
+
+        }
+
+        private void BtnRestaurar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Restaurar());
+        }
+    }
+}

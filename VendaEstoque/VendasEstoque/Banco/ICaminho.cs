15abd3b9bd86019ebfd05e8a42d291825ff707e8
rani69 <52800504+rani69@users.noreply.github.com>
2019-10-16 14:53:00 -0300

    Create ICaminho.cs

---
+++ VendaEstoque/VendasEstoque/Banco/ICaminho.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendasEstoque.Banco
+{
+    public interface ICaminho
+    {
+        string ObterCaminho(string NomeArquivoBanco);
+
+    }
+}
